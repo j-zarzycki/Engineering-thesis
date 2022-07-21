d619dcfd74a672f588916d9dd637428805ae1370
DESKTOP-50B3PKI\anubu <a.wolfus@gmail.com>
2022-07-21 14:16:42 +0200

    Added GET endpoints

--- kamre-backend/app.py
+++ kamre-backend/app.py
@@ -10,13 +10,29 @@ CORS(app)
 
 
 @app.route("/noContent", methods=['POST'])
-def no_content():
+def noContent():
     res = request.get_json()
     registered_date = parser.parse(res['registered_date'])
     activity_name = res['activity_name']
 
     svc.create_activity(registered_date, activity_name)
     return jsonify({'res': 'created entry'})
+
+
+@app.route("/getAll", methods=['GET'])
+def get_all():
+    names, dates = svc.get_all()
+
+    return jsonify({'names': names, 'dates': dates})
+
+
+@app.route("/getMonth", methods=['GET'])
+def get_month():
+    month = request.args.get('month')
+    year = request.args.get('year')
+    names, dates = svc.get_month(month, year)
+
+    return jsonify({'names': names, 'dates': dates})
 
 
 app.run(port=5000)
--- kamre-backend/services/db_actions.py
+++ kamre-backend/services/db_actions.py
@@ -1,4 +1,9 @@
 from data.Activity import Activity
+from dateutil import parser
+from data.months_dict import months_dict
+
+
+months_dict = months_dict()
 
 
 def create_activity(registered_date, activity_name):
@@ -10,3 +15,34 @@ def create_activity(registered_date, activity_name):
     activity.save()
 
     return activity
+
+
+def get_all():
+    activity_names = []
+    activity_dates = []
+
+    for entry in Activity.objects:
+        activity_names.append(entry.activity_name)
+        activity_dates.append(entry.registered_date)
+
+    return activity_names, activity_dates
+
+
+def get_month(month,year):
+    activity_names = []
+    activity_dates = []
+
+    start = parser.parse(f'{year}-{month}-1 00:00:00')
+    end_of_month = months_dict[int(month)]
+    try:
+        end = parser.parse(f'{year}-{month}-{end_of_month} 23:59:59')
+    except:
+        end = parser.parse(f'{year}-{month}-28 23:59:59')
+
+    activity = Activity.objects().filter(registered_date__gte=start, registered_date__lte=end)
+
+    for entry in activity:
+        activity_names.append(entry.activity_name)
+        activity_dates.append(entry.registered_date)
+
+    return activity_names, activity_dates
