5d46e07190362b160bdd02f45f52327679643604
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-07-23 17:17:44 +0200

    IN-91

--- kamre/src/pages/Walking.js
+++ kamre/src/pages/Walking.js
@@ -1,24 +1,44 @@
 /* eslint-disable */
 
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonContent, IonItem, IonLabel, IonList, IonPage} from '@ionic/react';
 import './Walking.css';
 import BackButton from "../components/BackButton";
+import SaveButton from "../components/SaveButton";
+import Header from "../components/Header";
 
 const Walking = () => {
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle class="ion-title">Spacer</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-            <div>
-                <BackButton defaultHref ="/home"/>
-            </div>
-        </IonContent>
-      </IonPage>
+        <IonPage>
+            <IonContent fullscreen class={"ion-padding-horizontal"}>
+                <div className={"title"}>
+                    <BackButton defaultHref="/home"/>
+                    <Header title={"Świadomy Spacer"}
+                            subtitle={"poniżej znajdziesz instrukcję, krok po kroku jak wykonać zadanie"}/>
+                </div>
+                <div>
+                    <IonList class="ion-text-wrap">
+                        <IonItem>
+                            <IonLabel>1. Wstań i załóż buty.</IonLabel>
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel>2. Najpierw spróbuj wyjść poza swoją posesję.</IonLabel>
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel className="ion-text-wrap">3. Gdy już to zrobisz, wybierz kierunek - lewo, prawo, może prosto?</IonLabel>
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel className="ion-text-wrap">4. W trakcie spaceru skup się na tym co słyszysz, widzisz, czujesz. Daj swoim emocjom Cię ponieść.</IonLabel>
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel>5. Po spacerze zanotuj swoje odczucia.</IonLabel>
+                        </IonItem>
+                    </IonList>
+                </div>
+                <div class="ion-text-center" >
+                    <SaveButton text={"Gotowe"} type={"submit"}/>
+                </div>
+            </IonContent>
+        </IonPage>
     );
   };
   
