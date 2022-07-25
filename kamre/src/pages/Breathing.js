bd70591d14406663153dc75b78df8c8f0a0d453a
Jedrzej Zarzycki <zarzycki.jed@gmail.com>
2022-07-25 18:44:04 +0200

    add breathing page

--- kamre/src/pages/Breathing.js
+++ kamre/src/pages/Breathing.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 
-import {IonContent, IonPage} from '@ionic/react';
+import { IonContent, IonPage, IonCard } from '@ionic/react';
 import './Breathing.css';
 import BackButton from "../components/BackButton";
 import Header from "../components/Header";
 import SaveButton from "../components/SaveButton";
 import breathingService from "../services/breathing.services"
+import { useCallback, useEffect, useState } from "react";
 
 
 const Breathing = () => {
@@ -20,19 +21,58 @@ const Breathing = () => {
         breathingService.CreateBreathing(dateTime, "Breath").then(res => (console.log("działa ten post"))).catch(err => console.log(err))
     }
 
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+    const placeholderText = ["wdech", "wydech"];
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        let interval;
+        if (running) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+
+            const timer = () => {
+                setIndex(prevIndex => {
+                    if (prevIndex === placeholderText.length - 1) {
+                        return 0;
+                    }
+                    return prevIndex + 1;
+                })
+            };
+            setInterval(timer, 7800);
+
+        } else if (!running) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [running]);
+
     return (
         <IonPage>
             <IonContent fullscreen class={"ion-padding-horizontal"}>
                 <div className={"title"}>
-                    <BackButton defaultHref="/home"/>
+                    <BackButton defaultHref="/home" />
                     <Header title={"Oddychanie"}
-                            subtitle={"poniżej znajdziesz instrukcję, krok po kroku jak wykonać zadanie"}/>
+                        subtitle={"poniżej znajdziesz instrukcję, krok po kroku jak wykonać zadanie"} />
                 </div>
-                <div>
-
-                </div>
+                <IonCard>
+                    <div className="stopwatch ion-text-center">
+                        <div className="numbers">
+                            <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+                            <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+                            <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+                        </div>
+                        <div className="message-container">
+                            <p className="message">{placeholderText[index]}</p>
+                        </div>
+                        <div className="buttons">
+                            <button onClick={() => setRunning(true)}>Start</button>
+                        </div>
+                    </div>
+                </IonCard>
                 <div className="ion-text-center">
-                    <SaveButton text={"Gotowe"} type={"submit"} onClick={createBreathing}/>
+                    <SaveButton text={"Gotowe"} type={"submit"} onClick={createBreathing} />
                 </div>
             </IonContent>
         </IonPage>
@@ -40,4 +80,3 @@ const Breathing = () => {
 };
 
 export default Breathing;
-  
