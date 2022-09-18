f55100d628dd8e18d1d13c9b2c9b9576c4597b46
Scribe <kwojtowicz2@edu.cdv.pl>
2022-09-18 05:50:46 +0200

    IN-236 content

---
+++ kamre/src/constants/SchulzTraining.constatns.ts
@@ -0,0 +1,4 @@
+const SCHULZ_TRAINING_URL =
+  "https://www.youtube-nocookie.com/embed/Bt6FF2Z5XU4";
+
+export default SCHULZ_TRAINING_URL;
---
+++ kamre/src/pages/SchulzTraining/SchulzTraining.component.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { IonContent, IonPage } from "@ionic/react";
+
+import BackButton from "@Components/BackButton";
+import Header from "@Components/Header";
+import SaveButton from "@Components/SaveButton";
+import SCHULZ_TRAINING_URL from "@Constants/SchulzTraining.constatns";
+
+interface IProps {
+  createSchulzTraining(): Promise<void>;
+}
+
+const SchulzTraining: React.FC<IProps> = (props: IProps) => {
+  const { createSchulzTraining } = props;
+  const videoUrl = SCHULZ_TRAINING_URL;
+
+  return (
+    <IonPage>
+      <IonContent fullscreen class="ion-padding-horizontal">
+        <div className="title">
+          <BackButton defaultHref="/home" />
+          <Header
+            title="Trening autogenny schulza"
+            subtitle="Czyli jak radzić sobie z trudnymi sytuacjami, zachowując spokój i panując nad emocjami."
+          />
+        </div>
+        <iframe
+          width="100%"
+          height="100%"
+          src={videoUrl}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+        />
+        <div className="ion-text-center">
+          <SaveButton
+            text="Gotowe"
+            type="submit"
+            onClick={createSchulzTraining()}
+          />
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SchulzTraining;
