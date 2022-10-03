202e8c2eacfd92de13c5eea55f20b884384b8606
Scribe <kwojtowicz2@edu.cdv.pl>
2022-10-03 17:32:22 +0200

    Added styles
    Schulz Training

--- kamre/src/pages/SchulzTraining/SchulzTraining.component.tsx
+++ kamre/src/pages/SchulzTraining/SchulzTraining.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IonContent, IonPage } from "@ionic/react";
 
+import "./SchulzTraining.style.scss";
 import BackButton from "@Components/BackButton";
 import Header from "@Components/Header";
 import SaveButton from "@Components/SaveButton";
@@ -17,26 +18,29 @@ const SchulzTraining: React.FC<IProps> = (props: IProps) => {
   return (
     <IonPage>
       <IonContent fullscreen class="ion-padding-horizontal">
-        <div className="title">
-          <BackButton defaultHref="/home" />
-          <Header
-            title="Trening autogenny schulza"
-            subtitle="Czyli jak radzić sobie z trudnymi sytuacjami, zachowując spokój i panując nad emocjami."
-          />
-        </div>
-        <iframe
-          width="100%"
-          height="100%"
-          src={videoUrl}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-        />
-        <div className="ion-text-center">
-          <SaveButton
-            text="Gotowe"
-            type="submit"
-            onClick={createSchulzTraining()}
-          />
+        <div className="s-training">
+          <div className="title">
+            <BackButton defaultHref="/home" />
+            <Header
+              title="Trening autogenny schulza"
+              subtitle="Czyli jak radzić sobie z trudnymi sytuacjami, zachowując spokój i panując nad emocjami."
+            />
+          </div>
+          <div className="video-container">
+            <iframe
+              className="embed"
+              src={videoUrl}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            />
+          </div>
+          <div className="ion-text-center">
+            <SaveButton
+              text="Gotowe"
+              type="submit"
+              onClick={createSchulzTraining()}
+            />
+          </div>
         </div>
       </IonContent>
     </IonPage>
