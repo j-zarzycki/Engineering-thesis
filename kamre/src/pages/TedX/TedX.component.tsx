e3c123b845851e2703e1fb9dfa4d337000f22a5f
Scribe <kwojtowicz2@edu.cdv.pl>
2022-10-03 17:13:01 +0200

    Styles added
    TedX activity

--- kamre/src/pages/TedX/TedX.component.tsx
+++ kamre/src/pages/TedX/TedX.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IonContent, IonPage } from "@ionic/react";
 
+import "./TedX.style.scss";
 import BackButton from "@Components/BackButton";
 import Header from "@Components/Header";
 import SaveButton from "@Components/SaveButton";
@@ -17,22 +18,25 @@ const TedX: React.FC<IProps> = (props: IProps) => {
   return (
     <IonPage>
       <IonContent fullscreen class="ion-padding-horizontal">
-        <div className="title">
-          <BackButton defaultHref="/home" />
-          <Header
-            title="Czy stres to czyste zło?"
-            subtitle="Sprwadź jak można rzucić pozytywne światło na stres, który odczuwamy."
-          />
-        </div>
-        <iframe
-          width="100%"
-          height="100%"
-          src={videoUrl}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-        />
-        <div className="ion-text-center">
-          <SaveButton text="Gotowe" type="submit" onClick={createTedX()} />
+        <div className="tedx">
+          <div className="title">
+            <BackButton defaultHref="/home" />
+            <Header
+              title="Czy stres to czyste zło?"
+              subtitle="Sprwadź jak można rzucić pozytywne światło na stres, który odczuwamy."
+            />
+          </div>
+          <div className="video-container">
+            <iframe
+              className="embed"
+              src={videoUrl}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            />
+          </div>
+          <div className="ion-text-center">
+            <SaveButton text="Gotowe" type="submit" onClick={createTedX()} />
+          </div>
         </div>
       </IonContent>
     </IonPage>
