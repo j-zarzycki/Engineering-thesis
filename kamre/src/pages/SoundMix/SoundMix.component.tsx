dadc068573295de1805f6f67f86d7c5044cb924e
Scribe <kwojtowicz2@edu.cdv.pl>
2022-10-05 17:43:26 +0200

    Added styles
    Piosenka

--- kamre/src/pages/SoundMix/SoundMix.component.tsx
+++ kamre/src/pages/SoundMix/SoundMix.component.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { IonContent, IonPage } from "@ionic/react";
 
+import "./SoundMix.style.scss";
 import BackButton from "@Components/BackButton";
 import Header from "@Components/Header";
 import SaveButton from "@Components/SaveButton";
-import SCHULZ_TRAINING_URL from "@Constants/SchulzTraining.constatns";
+import SOUND_MIX_URL from "@Constants/SoundMix.constatns";
 
 interface IProps {
   createSoundMix(): Promise<void>;
@@ -12,27 +13,36 @@ interface IProps {
 
 const SoundMix: React.FC<IProps> = (props: IProps) => {
   const { createSoundMix } = props;
-  const videoUrl = SCHULZ_TRAINING_URL;
+  const videoUrl = SOUND_MIX_URL;
 
   return (
     <IonPage>
       <IonContent fullscreen class="ion-padding-horizontal">
-        <div className="title">
-          <BackButton defaultHref="/home" />
-          <Header
-            title="Trening autogenny schulza"
-            subtitle="Czyli jak radzić sobie z trudnymi sytuacjami, zachowując spokój i panując nad emocjami."
-          />
-        </div>
-        <iframe
-          width="100%"
-          height="100%"
-          src={videoUrl}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-        />
-        <div className="ion-text-center">
-          <SaveButton text="Gotowe" type="submit" onClick={createSoundMix()} />
+        <div className="sound-mix">
+          <div className="title">
+            <BackButton defaultHref="/home" />
+            <Header
+              title="Relaksujące dźwięki"
+              subtitle="Chwila dla siebie wykorzystując dźwięki natury. Gdziekolwiek jesteś, możesz złapać odrobinę relaksu."
+            />
+          </div>
+          <div className="video-container">
+            <iframe
+              className="embed"
+              width="100%"
+              height="100%"
+              src={videoUrl}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            />
+          </div>
+          <div className="ion-text-center">
+            <SaveButton
+              text="Gotowe"
+              type="submit"
+              onClick={createSoundMix()}
+            />
+          </div>
         </div>
       </IonContent>
     </IonPage>
