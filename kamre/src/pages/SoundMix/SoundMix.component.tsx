08fedd8b273e895f0a801995bc5c75d6090004c4
Scribe <kwojtowicz2@edu.cdv.pl>
2022-10-16 11:10:54 +0200

    SoundMix update and fixes

--- kamre/src/pages/SoundMix/SoundMix.component.tsx
+++ kamre/src/pages/SoundMix/SoundMix.component.tsx
@@ -1,182 +1,227 @@
-import React, { useEffect, useState } from "react";
-import { IonContent, IonPage, useIonAlert } from "@ionic/react";
+import React from "react";
+import { IonContent, IonPage, IonLoading, IonToast } from "@ionic/react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { CSSTransition } from "react-transition-group";
+import { Swiper as SwiperType } from "swiper/types";
 
 // Import Swiper styles
-// import "swiper/css";
+import "swiper/css";
 
 import "./SoundMix.style.scss";
-import { SOUND_MIX_URL, SWIPE_ELEMENTS } from "@Constants/soundMix.constatns";
-import quote from "@Assets/what.png";
-import main from "@Assets/main.png";
+import HorizontalProgressBar from "@Components/HorizontalProgressBar";
 import BackButton from "@Components/BackButton";
 import ProceedButton from "@Components/ProceedButton";
+import SaveActivityButton from "@Components/SaveActivityButton";
+import CancelButton from "@Components/CancelButton";
 import Pet from "@Components/Pet";
+import { SOUND_MIX_URL } from "@Constants/soundMix.constatns";
 
 interface IProps {
   onCreateActivityWithNoContent(): Promise<void>;
-  onCreateActivityWithContent(activityContent: String): Promise<void>;
+  onCreateActivityWithContent(): void;
+  setToast(value: {}): void;
+  onProceedButtonClick(): void;
+  setSwiper(value: any): void;
+  onSlideChangeHandler(slide: SwiperType): void;
+  isLoading: boolean;
+  toast: any;
+  currentSlide: number;
+  swiper: any;
+  img: string;
+  slideElements: number;
 }
 
 const SoundMix: React.FC<IProps> = (props: IProps) => {
-  const { onCreateActivityWithNoContent, onCreateActivityWithContent } = props;
-  const [swiper, setSwiper] = useState<any>(null);
-  const [img, setImg] = useState("");
-  const [presentAlert] = useIonAlert();
-  const [showProceedButton, setShowProceedButton] = useState(true);
-  const slideElements = SWIPE_ELEMENTS;
+  const {
+    onCreateActivityWithNoContent,
+    onCreateActivityWithContent,
+    onProceedButtonClick,
+    onSlideChangeHandler,
+    setToast,
+    setSwiper,
+    isLoading,
+    toast,
+    swiper,
+    currentSlide,
+    img,
+    slideElements,
+  } = props;
   const videoUrl = SOUND_MIX_URL;
 
-  useEffect(() => {
-    setImg(main);
-  }, []);
-  const onProceedButtonClick = () => {
-    swiper?.slideNext();
-    if (swiper?.activeIndex === slideElements - 1) {
-      setImg(main);
-      setShowProceedButton(false);
-    }
-    if (swiper?.activeIndex === slideElements - 3) {
-      setImg(quote);
-    }
-  };
-  const onAlertButtonClick = (alertData: String) => {
-    onCreateActivityWithContent(alertData);
+  const renderLoader = () => {
+    return (
+      <IonLoading
+        cssClass="soundmix__loader"
+        isOpen={isLoading}
+        message="Zapisywanie, proszę czekać"
+      />
+    );
   };
 
-  const onProceedButtonClickWithContent = () => {
-    presentAlert({
-      header: "Dodaj swoje przemyślenia",
-      buttons: [
-        {
-          text: "OK",
-          handler: (alertData) => {
-            onAlertButtonClick(alertData.content);
-          },
-        },
-      ],
-      inputs: [
-        {
-          name: "content",
-          placeholder: "Wpisz je tutaj...",
-        },
-      ],
-    });
+  const renderToast = () => {
+    const { isOpen, message } = toast;
+    return (
+      <IonToast
+        isOpen={isOpen}
+        onDidDismiss={() => setToast({ isOpen: false, message: "" })}
+        message={message}
+        duration={2500}
+        position="top"
+      />
+    );
+  };
+
+  const renderHeader = () => {
+    if (swiper?.activeIndex === 4) return <div className="soundmix__header" />;
+
+    return (
+      <div className="soundmix__header">
+        <BackButton defaultHref="/home" />
+      </div>
+    );
+  };
+
+  const renderImage = () => {
+    if (swiper?.activeIndex === 3) {
+      return (
+        <div className="video">
+          <div className="video__container">
+            <iframe
+              className="video__embed"
+              src={videoUrl}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            />
+          </div>
+          <p className="video__source">
+            Źródło YouTube: Lofi Girl - lofi hip hop radio
+          </p>
+        </div>
+      );
+    }
+    return (
+      <Pet
+        src={img}
+        alt="Uśmiechnięta ośmiorniczka jpg"
+        height="265px"
+        paddingTop="20px"
+        paddingBottom="20px"
+      />
+    );
+  };
+
+  const renderHorizontalProgressBar = () => {
+    return (
+      <div className="soundmix__horizontal-progress-bar">
+        <HorizontalProgressBar
+          currentElement={currentSlide}
+          elements={slideElements}
+        />
+      </div>
+    );
+  };
+
+  const renderSwiper = () => {
+    return (
+      <div className="soundmix__swiper">
+        <Swiper
+          effect="fade"
+          centeredSlides
+          slidesPerView={1}
+          onSwiper={(swiperData) => setSwiper(swiperData)}
+          onSlideChange={(slide) => onSlideChangeHandler(slide)}
+        >
+          <SwiperSlide>
+            <div className="swiper-slide__wrapper">
+              <h4 className="swiper-slide__header">Muzyka dla duszy </h4>
+              <p className="swiper-slide__paragraph">
+                Muzyka jest jedną z najlepszych rzeczy dla serca i duszy -
+                pomaga łagodzić ból, poprawia nastrój i budzi w nas pozytywne
+                emocje. W skrócie, czyni nas szczęśliwszymi!
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide__wrapper">
+              <h4 className="swiper-slide__header">O co chodzi?</h4>
+              <p className="swiper-slide__paragraph">
+                Puść swoją ulubioną piosenkę lub playlistę! Oglądałeś Stranger
+                Thnigs? Czas otwórzyć własny magiczny portal i uciec w
+                przyjemniejsze miejsce.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide__wrapper">
+              <h4 className="swiper-slide__header">Nie ograniczaj się!</h4>
+              <p className="swiper-slide__paragraph">
+                Spróbuj poczuć muzykę, każde jej brzmienie. Jeśli masz ochotę,
+                rozluźnij mięśnie i zacznij poruszać w rytm muzyki. Poczuj
+                napływ endorfin!
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide__wrapper">
+              <p className="swiper-slide__paragraph">
+                Szukasz muzycznych inspiracji? Sprawdź naszą playlistę!
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide__wrapper">
+              <h4 className="swiper-slide__header">Przemyślenia</h4>
+              <p className="swiper-slide__paragraph">
+                Jakie odczucia towarzyszyły Ci podczas słuchania muzyki? Podziel
+                się swoimi przemyśleniami.
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    );
+  };
+
+  const renderButtons = () => {
+    if (swiper?.activeIndex >= 4)
+      return (
+        <div className="soundmix__final-buttons">
+          <CancelButton
+            onClick={onCreateActivityWithNoContent}
+            title="Zakończ"
+          />
+          <SaveActivityButton
+            title="Dodaj"
+            onClick={onCreateActivityWithContent}
+          />
+        </div>
+      );
+
+    return <ProceedButton title="Dalej!" onClick={onProceedButtonClick} />;
+  };
+
+  const renderContext = () => {
+    return (
+      <>
+        {renderImage()}
+        {renderHorizontalProgressBar()}
+        {renderSwiper()}
+        {renderButtons()}
+      </>
+    );
   };
 
   return (
     <IonPage>
       <IonContent fullscreen class="ion-padding-horizontal">
-        <div className="sound-mix">
-          <BackButton defaultHref="/home" />
-          <div className="sound-mix__wrapper">
-            <div className="conscious-shower__swiper">
-              <Swiper
-                allowTouchMove={false}
-                effect="fade"
-                centeredSlides
-                slidesPerView={1}
-                onSwiper={(swiperData) => setSwiper(swiperData)}
-              >
-                <SwiperSlide>
-                  <div className="swiper-slide__wrapper">
-                    <Pet
-                      src={img}
-                      alt="Ośmiorniczka ze znakiem zapytania jpg"
-                      height="200px"
-                      paddingTop="20px"
-                      paddingBottom="20px"
-                    />
-                    <h4 className="swiper-slide__header">Muzyka dla duszy</h4>
-                    <p className="swiper-slide__description">
-                      Muzyka jest jedną z najlepszych rzeczy dla serca i duszy -
-                      pomaga łagodzić ból, poprawia nastrój i budzi w nas
-                      pozytywne emocje. W skrócie, czyni nas szczęśliwszymi!
-                    </p>
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="swiper-slide__wrapper">
-                    <Pet
-                      src={img}
-                      alt="Ośmiorniczka ze znakiem zapytania jpg"
-                      height="200px"
-                      paddingTop="20px"
-                      paddingBottom="20px"
-                    />
-                    <h4 className="swiper-slide__header">
-                      O co chodzi w ćwiczeniu?
-                    </h4>
-                    <p className="swiper-slide__description">
-                      Puść swoją ulubioną piosenkę lub playlistę! Oglądałeś
-                      Stranger Thnigs? Czas otwórzyć własny magiczny portal i
-                      uciec w przyjemniejsze miejsce. Spróbuj poczuć muzykę,
-                      każde jej brzmienie. Jeśli masz ochotę, rozluźnij mięśnie
-                      i zacznij poruszać w rytm muzyki. Poczuj napływ endorfin!
-                    </p>
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="swiper-slide__wrapper">
-                    <div className="video-container">
-                      <iframe
-                        className="video-container__embed"
-                        src={videoUrl}
-                        title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-                      />
-                    </div>
-                    <p className="swiper-slide__description">
-                      Szukasz muzycznych inspiracji? Sprawdź naszą playlistę!
-                    </p>
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="swiper-slide__wrapper">
-                    <Pet
-                      src={img}
-                      alt="Uśmiechnięta ośmiorniczka jpg"
-                      height="200px"
-                      paddingTop="20px"
-                      paddingBottom="20px"
-                    />
-                    <h4 className="swiper-slide__header">
-                      Gratulacje, udało&nbsp;Ci&nbsp;się!
-                    </h4>
-                  </div>
-                </SwiperSlide>
-              </Swiper>
-            </div>
-            <div className="sound-mix__buttons">
-              {showProceedButton && (
-                <ProceedButton title="Dalej" onClick={onProceedButtonClick} />
-              )}
-
-              <CSSTransition
-                in={!showProceedButton}
-                timeout={300}
-                classNames="swiper__proceed-buttons"
-                unmountOnExit
-                onEnter={() => setShowProceedButton(false)}
-                onExited={() => setShowProceedButton(true)}
-              >
-                <div>
-                  <ProceedButton
-                    title="Dodaj przemyślenia"
-                    onClick={onProceedButtonClickWithContent}
-                  />
-                  <ProceedButton
-                    title="Zakończ"
-                    onClick={onCreateActivityWithNoContent}
-                  />
-                </div>
-              </CSSTransition>
-            </div>
-          </div>
+        <div className="soundmix">
+          {renderToast()}
+          {renderLoader()}
+          {renderHeader()}
+          <div className="soundmix__wrapper">{renderContext()}</div>
         </div>
       </IonContent>
     </IonPage>
   );
 };
+
 export default SoundMix;
--- kamre/src/pages/SoundMix/SoundMix.container.tsx
+++ kamre/src/pages/SoundMix/SoundMix.container.tsx
@@ -1,34 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Swiper } from "swiper/types";
 
 import { getFullDateWithTime } from "@Utils/date";
+import { createNote } from "@Store/slices/noteSlice";
 import apiService from "@Services/api.service";
+import useAppDispatch from "@Hooks/useAppDispatch";
+import { SWIPE_ELEMENTS } from "@Constants/soundMix.constatns";
+import MainImg from "@Assets/main.png";
+import quote from "@Assets/what.png";
 import SoundMix from "./SoundMix.component";
 
 const SoundMixContainer: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [swiper, setSwiper] = useState<any>(null);
+  const [img, setImg] = useState("");
+  const [toast, setToast] = useState({ isOpen: false, message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const slideElements = SWIPE_ELEMENTS;
   const currentDateWithTime: String = getFullDateWithTime();
+  const history = useHistory();
+  const dispatch = useAppDispatch();
 
   const createSoundMixWithNoContent = async () => {
+    setIsLoading(true);
     await apiService
       .CreateActivityWithNoContent(currentDateWithTime, "Piosenka")
-      .then((data) => console.log("data = ", data))
-      .catch((err) => console.log("err = ", err));
+      .then(() => {
+        setToast({ isOpen: true, message: "Pomyślnie zapisano!" });
+      })
+      .finally(() => {
+        setIsLoading(false);
+        history.replace("/home");
+      })
+      .catch(() =>
+        setToast({
+          isOpen: true,
+          message: "Wystąpił błąd podczas zapisywania.",
+        }),
+      );
   };
 
-  const createSoundMixWithContent = async (activityContent: String) => {
-    await apiService
-      .CreateActivityWithContent(
-        currentDateWithTime,
-        activityContent,
-        "Piosenka",
-      )
-      .then((data) => console.log("data = ", data))
-      .catch((err) => console.log("err = ", err));
+  const createSoundMixWithContent = () => {
+    dispatch(
+      createNote({
+        contentName: "Piosenka",
+        title: "Piosenka",
+        description: "Zapisz, jakie uczucia wywołała w Tobie piosenka.",
+        hiddenDescription: "",
+      }),
+    );
+
+    history.push("/note");
   };
+
+  const onProceedButtonClick = () => {
+    swiper?.slideNext();
+
+    setCurrentSlide(swiper?.activeIndex);
+    if (swiper?.activeIndex === slideElements - 4) {
+      setImg(quote);
+    }
+    if (swiper?.activeIndex === slideElements - 1) {
+      setImg(MainImg);
+    }
+  };
+
+  const onSlideChangeHandler = (slide: Swiper) => {
+    setCurrentSlide(slide?.activeIndex);
+    setImg(MainImg);
+    if (slide?.activeIndex === 1 || slide?.activeIndex === 2) {
+      setImg(quote);
+    }
+    if (slide?.activeIndex === slideElements - 1) {
+      setImg(MainImg);
+    }
+  };
+
+  useEffect(() => {
+    setImg(MainImg);
+  }, []);
 
   return (
     <SoundMix
       onCreateActivityWithNoContent={createSoundMixWithNoContent}
       onCreateActivityWithContent={createSoundMixWithContent}
+      onProceedButtonClick={onProceedButtonClick}
+      setToast={setToast}
+      setSwiper={setSwiper}
+      currentSlide={currentSlide}
+      isLoading={isLoading}
+      toast={toast}
+      swiper={swiper}
+      img={img}
+      slideElements={slideElements}
+      onSlideChangeHandler={onSlideChangeHandler}
     />
   );
 };
