8d635cdd218d12692d07c6aee643060e387874dd
DESKTOP-50B3PKI\anubu <a.wolfus@gmail.com>
2022-10-27 16:53:47 +0200

    Updated get methods for all categories, recommended, updated code to work with new collection, added a way to guarantee uniqueness of recommended activities when using chat, updated recovery codes to generate the code once (still only valid 12h, but when a user requests a new code while the other one still exists they get the old one)

--- kamre-backend/data/Recent.py
+++ kamre-backend/data/Recent.py
@@ -9,5 +9,5 @@ class Recent(db.EmbeddedDocument):
     Aktywne_count = db.IntField(default=0)
     Bierne_count = db.IntField(default=0)
     ZmianaMyslenia_count = db.IntField(default=0)
-    PozytywneEmocje_count = db.IntField(defualt=0)
+    PozytywneEmocje_count = db.IntField(default=0)
     total = db.IntField(default=0)
